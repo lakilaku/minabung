5b3d2c683000f43fb605d80cb9411cfb8f5f72a4
RakaaMahendra <rakamahendra900@gmail.com>
2025-02-28 12:58:41 +0800

    feat: completed register, login, updatedprofile

--- servers/app.js
+++ servers/app.js
@@ -1,14 +1,41 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { userResolvers, userTypeDefs } from "./schemas/Users.js";
+import { verifyToken } from "./helpers/jwt.js";
 
 const server = new ApolloServer({
   typeDefs: [userTypeDefs],
   resolvers: [userResolvers],
 });
 
-const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 },
-});
+async function startServer() {
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: process.env.PORT || 4000 },
+    context: async ({ req }) => {
+      return {
+        authentication: async () => {
+          const token = req.headers.authorization || "";
+          if (!token) {
+            throw new Error("No token provided");
+          }
 
-console.log(`🚀  Server ready at: ${url}`);
+          const [type, tkn] = token.split(" ");
+          if (type !== "Bearer") {
+            throw new Error("Invalid token type");
+          }
+
+          if (!tkn) {
+            throw new Error("No token provided");
+          }
+
+          const payload = verifyToken(tkn);
+          return payload;
+        },
+      };
+    },
+  });
+
+  console.log(`🚀 Server ready at: ${url}`);
+}
+
+startServer();
