d1b4154b7c19cefd72f1ef6c224704db6342db4d
Hendyvelarius <h.velarius@gmail.com>
2025-02-28 11:09:13 +0700

    checkpoint: added Group Model and Group Schemas for all group functions and income

---
+++ servers/models/GroupModel.js
@@ -0,0 +1,180 @@
+export default class GroupModel {
+  static collection() {
+    return database.collection("groups");
+  }
+
+  static async getGroupById(id) {
+    return await this.collection().findOne({ _id: id });
+  }
+
+  static async getGroupByUserId(userId) {
+    return await this.collection().find({ members: userId }).toArray();
+  }
+
+  static async findGroupByInvite(invite) {
+    return await this.collection().findOne({ invite: invite });
+  }
+
+  static async createGroup(auth, group) {
+    const auth = await context.authentication();
+    const newGroup = {
+      name: group.name,
+      description: group.description,
+      members: [
+        {
+          _id: auth._id,
+          name: auth.name,
+          role: "Owner",
+        },
+      ],
+    };
+    const result = await this.collection().insertOne(newGroup);
+    if (result.insertedCount === 1) {
+      throw new Error("Failed to create group");
+    }
+    return newGroup;
+  }
+
+  static async joinGroup(invite) {
+    const auth = await context.authentication();
+    const group = await this.findGroupByInvite(invite);
+    if (!group) {
+      throw new Error("Group not found");
+    }
+    const result = await this.collection().updateOne(
+      { _id: group._id },
+      { $push: { members: { _id: auth._id, name: auth.name, role: "Member" } } }
+    );
+    if (result.modifiedCount === 1) {
+      throw new Error("Failed to join group");
+    }
+    return group;
+  }
+
+  static async updateGroup(auth, id, name, description) {
+    const group = await this.getGroupById(id);
+    if (!group) {
+      throw new Error("Group not found");
+    }
+    let member = group.members.find(
+      (member) => member._id.toString() === auth._id.toString()
+    );
+    if (!member) {
+      throw new Error("You are not a member of this group");
+    }
+    if (member.role !== "Owner" || member.role !== "Admin") {
+      throw new Error("You are not the admin of this group");
+    }
+    const result = await this.collection().updateOne(
+      { _id: id },
+      { $set: { name, description } }
+    );
+    if (result.modifiedCount === 1) {
+      throw new Error("Failed to update group");
+    }
+    return group;
+  }
+
+  static async deleteGroup(auth, id) {
+    const group = await this.getGroupById(id);
+    if (!group) {
+      throw new Error("Group not found");
+    }
+    let member = group.members.find(
+      (member) => member._id.toString() === auth._id.toString()
+    );
+    if (!member) {
+      throw new Error("You are not a member of this group");
+    }
+    if (member.role !== "Owner") {
+      throw new Error("Only owners can delete the group");
+    }
+    const result = await this.collection().deleteOne({ _id: id });
+    if (result.deletedCount === 1) {
+      throw new Error("Failed to delete group");
+    }
+    return group;
+  }
+
+  static async addIncome(auth, groupId, name, note, amount, date) {
+    const group = await this.getGroupById(groupId);
+    if (!group) {
+      throw new Error("Group not found");
+    }
+    let member = group.members.find(
+      (member) => member._id.toString() === auth._id.toString()
+    );
+    if (!member) {
+      throw new Error("You are not a member of this group");
+    }
+    const income = {
+      name,
+      note,
+      amount,
+      date,
+    };
+    const result = await this.collection().updateOne(
+      { _id: groupId },
+      { $push: { incomes: income } }
+    );
+    if (result.modifiedCount < 1) {
+      throw new Error("Failed to add income");
+    }
+    return income;
+  }
+
+  static async updateIncome(auth, groupId, id, name, note, amount, date) {
+    const group = await this.getGroupById(groupId);
+    if (!group) {
+      throw new Error("Group not found");
+    }
+    let member = group.members.find(
+      (member) => member._id.toString() === auth._id.toString()
+    );
+    if (!member) {
+      throw new Error("You are not a member of this group");
+    }
+    const income = group.incomes.find((income) => income._id.toString() === id);
+    if (!income) {
+      throw new Error("Income not found");
+    }
+    const result = await this.collection().updateOne(
+      { _id: groupId, "incomes._id": id },
+      {
+        $set: {
+          "incomes.$.name": name,
+          "incomes.$.note": note,
+          "incomes.$.amount": amount,
+          "incomes.$.date": date,
+        },
+      }
+    );
+    if (result.modifiedCount < 1) {
+      throw new Error("Failed to update income");
+    }
+  }
+
+  static async deleteIncome(auth, groupId, id) {
+    const group = await this.getGroupById(groupId);
+    if (!group) {
+      throw new Error("Group not found");
+    }
+    let member = group.members.find(
+      (member) => member._id.toString() === auth._id.toString()
+    );
+    if (!member) {
+      throw new Error("You are not a member of this group");
+    }
+    const income = group.incomes.find((income) => income._id.toString() === id);
+    if (!income) {
+      throw new Error("Income not found");
+    }
+    const result = await this.collection().updateOne(
+      { _id: groupId },
+      { $pull: { incomes: { _id: id } } }
+    );
+    if (result.modifiedCount < 1) {
+      throw new Error("Failed to delete income");
+    }
+  }
+}
--- servers/schemas/Groups.js
+++ servers/schemas/Groups.js
@@ -3,7 +3,7 @@ type Group {
     _id: ID
     name: String
     description: String
-    members: [User]
+    members: [UserGroup]
     incomes: [Income]
     expenses: [Expense]
     budgets: [Budget]
@@ -16,6 +16,12 @@ type Budget {
     limit: Float
     icon: String
     color: String
+}
+
+type UserGroup {
+  _id: ID!
+  name: String
+  role: String
 }
 
 type Income {
@@ -38,14 +44,12 @@ type Expense {
 type Query {
     getGroupById(id: ID!): Group
     getGroupByUserId(userId: ID!): [Group]
-    getIncomesByGroupId(groupId: ID!): [Income]
-    getExpensesByGroupId(groupId: ID!): [Expense]
-    getBudgetsByGroupId(groupId: ID!): [Budget]
     findGroupByInvite(invite: String!): Group
+    findIncomeById(id: ID!): Income
 }
 
 type Mutation {
-    createGroup(name: String!, description: String, members: [ID], incomes: [IncomeInput], expenses: [ExpenseInput], budgets: [BudgetInput]): Group
+    createGroup(name: String!, description: String): Group
     joinGroup(invite: String!): Group
     updateGroup(id: ID!, name: String, description: String): Group
     deleteGroup(id: ID!): Group
@@ -57,6 +61,7 @@ type Mutation {
     deleteExpense(id: ID!): Expense
     addBudget(groupId: ID!, name: String!, limit: Float!, icon: String, color: String): Budget
     updateBudget(id: ID!, name: String, limit: Float, icon: String, color: String): Budget
+    deleteBudget(id: ID!): Budget
 }
 `;
 
@@ -68,40 +73,20 @@ const resolvers = {
     getGroupByUserId: async (_, { userId }) => {
       return await GroupModel.collection().find({ members: userId }).toArray();
     },
-    getIncomesByGroupId: async (_, { groupId }) => {
-      return await IncomeModel.collection()
-        .find({ groupId: groupId })
-        .toArray();
-    },
-    getExpensesByGroupId: async (_, { groupId }) => {
-      return await ExpenseModel.collection()
-        .find({ groupId: groupId })
-        .toArray();
-    },
-    getBudgetsByGroupId: async (_, { groupId }) => {
-      return await BudgetModel.collection()
-        .find({ groupId: groupId })
-        .toArray();
-    },
     findGroupByInvite: async (_, { invite }) => {
       return await GroupModel.collection().findOne({ invite: invite });
+    },
+    findIncomeById: async (_, { id }) => {
+      return await GroupModel.collection().findOne({ "incomes._id": id });
     },
   },
 
   Mutation: {
-    createGroup: async (
-      _,
-      { name, description, members, incomes, expenses, budgets },
-      context
-    ) => {
+    createGroup: async (_, { name, description }, context) => {
       const auth = await context.authentication();
       const group = {
         name,
         description,
-        members,
-        incomes,
-        expenses,
-        budgets,
       };
       const result = await GroupModel.createGroup(auth, group);
       return result;
@@ -119,6 +104,36 @@ const resolvers = {
     deleteGroup: async (_, { id }) => {
       const auth = await context.authentication();
       const result = await GroupModel.deleteGroup(auth, id);
+      return result;
+    },
+    addIncome: async (_, { groupId, name, note, amount, date }, context) => {
+      const auth = await context.authentication();
+      const result = await GroupModel.addIncome(
+        auth,
+        groupId,
+        name,
+        note,
+        amount,
+        date
+      );
+      return result;
+    },
+    updateIncome: async (_, { id, groupId, name, note, amount, date }) => {
+      const auth = await context.authentication();
+      const result = await GroupModel.updateIncome(
+        auth,
+        groupId,
+        id,
+        name,
+        note,
+        amount,
+        date
+      );
+      return result;
+    },
+    deleteIncome: async (_, { groupId, id }) => {
+      const auth = await context.authentication();
+      const result = await GroupModel.deleteIncome(auth, id);
       return result;
     },
   },
